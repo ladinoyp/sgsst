8e603cec957f8fab5b88774315959597ece98cff
ypladinoc <ypladinoc@eafit.edu.co>
2024-12-01 08:13:34 -0500

    welcome, register, login, homev2

--- src/app/constants/routes.ts
+++ src/app/constants/routes.ts
@@ -1,4 +1,8 @@
 export const PUBLIC_ROUTES ={ 
+    WELCOME : '/pages/welcome',
     LOGIN : '/pages/login',
     REGISTER : '/pages/register',
 } 
+export const PRIVATE_ROUTES={
+    HOME : '/pages/home',
+}
